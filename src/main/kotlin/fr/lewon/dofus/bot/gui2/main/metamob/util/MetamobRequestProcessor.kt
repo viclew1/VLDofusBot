6cdac577048a030eea1fa550c0030f429501d846
viclew <viclew@gmail.com>
2022-10-03 22:05:20 +0200

    Started character visual card

--- src/main/kotlin/fr/lewon/dofus/bot/gui2/main/metamob/util/MetamobRequestProcessor.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/gui2/main/metamob/util/MetamobRequestProcessor.kt
@@ -1,25 +1,18 @@
 package fr.lewon.dofus.bot.gui2.main.metamob.util
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.readValue
 import fr.lewon.dofus.bot.gui2.main.metamob.model.MetamobMonster
 import fr.lewon.dofus.bot.gui2.main.metamob.model.MetamobMonsterUpdate
 import fr.lewon.dofus.bot.util.filemanagers.impl.MetamobConfigManager
+import fr.lewon.dofus.bot.util.http.AbstractRequestProcessor
 import java.awt.image.BufferedImage
 import java.net.HttpURLConnection
 import java.net.URL
 import javax.imageio.ImageIO
 
-object MetamobRequestProcessor {
+object MetamobRequestProcessor : AbstractRequestProcessor("https://api-metamob.fr") {
 
-    private const val BASE_URI = "https://api.metamob.fr/"
     private const val USERS = "/utilisateurs/"
     private const val MONSTERS = "/monstres/"
-
-    private val objectMapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
 
     @Synchronized
     fun getAllMonsters(): List<MetamobMonster>? {
@@ -41,7 +34,7 @@ object MetamobRequestProcessor {
     }
 
     @Synchronized
-    fun checkParameters(): Boolean {
+    override fun checkParameters(): Boolean {
         val co = buildConnection("$USERS${getMetamobPseudo()}$MONSTERS")
         co.requestMethod = "PUT"
         writeBody(Object(), co)
@@ -51,45 +44,13 @@ object MetamobRequestProcessor {
         return co.responseCode == 200
     }
 
-    private inline fun <reified T> putForObject(uri: String, body: Any): T? {
-        if (!checkParameters()) {
-            return null
-        }
-        val co = buildConnection(uri)
-        co.requestMethod = "PUT"
-        writeBody(body, co)
-        return readResponse(co)
-    }
-
-    private fun buildConnection(uri: String): HttpURLConnection {
-        val co = URL("$BASE_URI$uri").openConnection() as HttpURLConnection
+    override fun setRequestProperties(co: HttpURLConnection) {
         co.setRequestProperty("HTTP-X-APIKEY", "59f661-004e81-cf6352-478f78-01f8df")
         co.setRequestProperty("HTTP-X-USERKEY", MetamobConfigManager.readConfig().metamobUniqueID)
-        return co
     }
 
     private fun getMetamobPseudo(): String {
         return MetamobConfigManager.readConfig().metamobPseudo ?: ""
     }
 
-    private fun writeBody(bodyObj: Any, connection: HttpURLConnection) {
-        val bodyJson = objectMapper.writeValueAsString(bodyObj)
-        connection.doOutput = true
-        val os = connection.outputStream
-        os.write(bodyJson.toByteArray(Charsets.UTF_8), 0, bodyJson.length)
-    }
-
-    private inline fun <reified T> readResponse(connection: HttpURLConnection): T {
-        connection.responseCode
-        return objectMapper.readValue(connection.inputStream.readAllBytes())
-    }
-
-    private inline fun <reified T> getForObject(uri: String): T? {
-        if (!checkParameters()) {
-            return null
-        }
-        val co = buildConnection(uri)
-        co.requestMethod = "GET"
-        return readResponse(co)
-    }
 }
--- src/main/kotlin/fr/lewon/dofus/bot/gui2/main/scripts/ScriptsContent.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/gui2/main/scripts/ScriptsContent.kt
@@ -1,5 +1,8 @@
 package fr.lewon.dofus.bot.gui2.main.scripts
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
@@ -7,6 +10,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import fr.lewon.dofus.bot.gui2.main.scripts.characters.CharactersListContent
+import fr.lewon.dofus.bot.gui2.main.scripts.characters.edit.CharacterEditionContent
+import fr.lewon.dofus.bot.gui2.main.scripts.scripts.ScriptTab
+import fr.lewon.dofus.bot.gui2.main.scripts.scripts.ScriptTabsUIUtil
 import fr.lewon.dofus.bot.gui2.main.scripts.scripts.ScriptsTabsContent
 import fr.lewon.dofus.bot.gui2.main.scripts.scripts.tabcontent.logs.LogsCharacterTabContent
 import fr.lewon.dofus.bot.gui2.main.scripts.status.StatusBarContent
@@ -23,7 +29,19 @@ fun ScriptsContent() {
                 ScriptsTabsContent()
             }
             Divider(Modifier.fillMaxHeight().width(1.dp), color = AppColors.backgroundColor)
-            LogsCharacterTabContent()
+            Column(Modifier.fillMaxSize()) {
+                AnimatedVisibility(
+                    visible = ScriptTabsUIUtil.getCurrentTab() == ScriptTab.INDIVIDUAL,
+                    enter = expandVertically(expandFrom = Alignment.Top),
+                    exit = shrinkVertically(shrinkTowards = Alignment.Top),
+                    modifier = Modifier.heightIn(max = 500.dp)
+                ) {
+                    CharacterEditionContent()
+                }
+                Row(Modifier.fillMaxHeight()) {
+                    LogsCharacterTabContent()
+                }
+            }
         }
         Row(Modifier.align(Alignment.BottomCenter)) {
             StatusBarContent()
