25202d54f17832efd5bc2b4e195c0f180d2314ef
viclew <viclew@gmail.com>
2022-10-12 20:42:18 +0200

    Improved metamob monsters display

--- src/main/kotlin/fr/lewon/dofus/bot/gui2/main/metamob/monsters/MetamobMonstersContent.kt
+++ src/main/kotlin/fr/lewon/dofus/bot/gui2/main/metamob/monsters/MetamobMonstersContent.kt
@@ -1,10 +1,13 @@
 package fr.lewon.dofus.bot.gui2.main.metamob.monsters
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +18,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import fr.lewon.dofus.bot.gui2.custom.CommonText
 import fr.lewon.dofus.bot.gui2.custom.CustomShapes
@@ -25,6 +27,7 @@ import fr.lewon.dofus.bot.gui2.main.metamob.MetamobHelperUIUtil
 import fr.lewon.dofus.bot.gui2.util.AppColors
 import fr.lewon.dofus.bot.windowState
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MetamobMonstersContent() {
     Column(Modifier.fillMaxSize().padding(5.dp).grayBoxStyle()) {
@@ -40,7 +43,7 @@ fun MetamobMonstersContent() {
                     refreshing = refreshing
                 )
             }
-            LaunchedEffect(Unit) {
+            LaunchedEffect(true) {
                 Thread {
                     try {
                         refreshing.value = true
@@ -52,12 +55,9 @@ fun MetamobMonstersContent() {
             }
             CommonText("Monsters", modifier = Modifier.padding(10.dp), fontWeight = FontWeight.SemiBold)
         }
-        val size = remember { mutableStateOf(IntSize(1000, 1000)) }
-        val state = rememberScrollState()
+        val state = rememberLazyListState()
         Box(Modifier.fillMaxSize().padding(5.dp)) {
-            Column(Modifier.fillMaxSize().padding(end = 8.dp)
-                .onGloballyPositioned { size.value = it.size }
-                .verticalScroll(state)) {
+            Column(Modifier.fillMaxSize().padding(end = 8.dp)) {
                 if (MetamobHelperUIUtil.getMonsters().isEmpty()) {
                     val errorMessage = MetamobHelperUIUtil.getErrorMessage()
                     if (errorMessage.isNotEmpty()) {
@@ -66,24 +66,15 @@ fun MetamobMonstersContent() {
                         CommonText("No monster to display")
                     }
                 } else {
-                    val chunkSize = 4
-                    val monsterRows = MetamobHelperUIUtil.getFilteredMonsters().chunked(chunkSize)
-                    for (monsterRow in monsterRows) {
-                        Row(Modifier.fillMaxWidth()) {
-                            for (i in 0 until chunkSize) {
-                                val monster = monsterRow.getOrNull(i)
-                                val boxModifier = if (monster != null) {
-                                    Modifier.onGloballyPositioned {
-                                        if (it.positionInWindow().y < windowState.size.height.value) {
-                                            MetamobHelperUIUtil.loadImagePainter(monster)
-                                        }
-                                    }
-                                } else Modifier
-                                Box(boxModifier.fillMaxWidth().height(110.dp).weight(1f).padding(5.dp)) {
-                                    if (monster != null) {
-                                        MonsterCardContent(monster)
-                                    }
+                    val filteredMonsters = MetamobHelperUIUtil.getFilteredMonsters()
+                    LazyVerticalGrid(cells = GridCells.Adaptive(200.dp), state = state) {
+                        items(filteredMonsters) { monster ->
+                            Box(Modifier.onGloballyPositioned {
+                                if (it.positionInWindow().y < windowState.size.height.value) {
+                                    MetamobHelperUIUtil.loadImagePainter(monster)
                                 }
+                            }.height(110.dp).padding(5.dp)) {
+                                MonsterCardContent(monster)
                             }
                         }
                     }
