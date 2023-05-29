18c2c2f5620e6e9f85b2547d5947592189006df5
100nom <forgemagie.dofus@gmail.com>
2023-05-30 00:59:23 +0200

    HarvestTask correction

--- VLDofusBotApp/src/main/kotlin/fr/lewon/dofus/bot/gui/main/exploration/ExplorationExplorerUIState.kt
+++ VLDofusBotApp/src/main/kotlin/fr/lewon/dofus/bot/gui/main/exploration/ExplorationExplorerUIState.kt
@@ -11,6 +11,10 @@ data class ExplorationExplorerUIState(
         ExploreAreaScriptBuilder.stopWhenQuestMonsterFoundParameter to false.toString(),
         ExploreAreaScriptBuilder.killEverythingParameter to false.toString(),
         ExploreAreaScriptBuilder.runForeverParameter to false.toString(),
-        ExploreAreaScriptBuilder.searchedMonsterParameter to ""
+        ExploreAreaScriptBuilder.searchedMonsterParameter to "",
+        ExploreAreaScriptBuilder.harvestParameter to false.toString(),
+        ExploreAreaScriptBuilder.harvestAllParameter to false.toString(),
+        ExploreAreaScriptBuilder.harvestJobParameter to ""
+
     )
 )
