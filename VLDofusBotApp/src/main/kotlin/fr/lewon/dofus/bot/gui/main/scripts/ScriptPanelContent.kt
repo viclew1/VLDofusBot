30d926e92e3c1d4c721d8b45c385b159236f728e
viclew <viclew@gmail.com>
2023-09-24 17:25:29 +0200

    ctrl + enter starts the script

--- VLDofusBotApp/src/main/kotlin/fr/lewon/dofus/bot/gui/main/scripts/ScriptPanelContent.kt
+++ VLDofusBotApp/src/main/kotlin/fr/lewon/dofus/bot/gui/main/scripts/ScriptPanelContent.kt
@@ -8,14 +8,28 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import fr.lewon.dofus.bot.gui.main.scripts.parameters.ScriptParametersContent
 import fr.lewon.dofus.bot.gui.main.scripts.scriptinfo.CurrentScriptInfoContent
 import fr.lewon.dofus.bot.gui.main.scripts.selector.ScriptSelectorContent
+import fr.lewon.dofus.bot.gui.main.scripts.selector.ScriptSelectorUIUtil
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ScriptPanelContent() {
-    Column {
+    Column(Modifier.onPreviewKeyEvent { keyEvent ->
+        if (keyEvent.isCtrlPressed && keyEvent.key in listOf(Key.Enter, Key.NumPadEnter)) {
+            if (!ScriptsUiUtil.isScriptStarted() && ScriptSelectorUIUtil.uiState.value.isStartButtonEnabled) {
+                ScriptsUiUtil.toggleScript()
+                true
+            } else false
+        } else false
+    }) {
         ScriptSelectorContent()
         val parameters = ScriptsUiUtil.getUiStateValue().currentScriptBuilder.getParameters()
         Column(Modifier.fillMaxHeight().weight(1f)) {
